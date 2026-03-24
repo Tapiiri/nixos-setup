c4a770a9b8182567f295bc027ccbf4ebb8336eea
Tapiiri <ilmari@tarpia.fi>
2026-03-24 15:33:33 +0200

    feat: enable Thunderbird package installation in home configuration

--- home/modules/thunderbird.nix
+++ home/modules/thunderbird.nix
@@ -9,6 +9,8 @@ in {
   options.my.thunderbird.enable = mkEnableOption "Thunderbird email client";
 
   config = mkIf config.my.thunderbird.enable {
+    home.packages = [pkgs.thunderbird];
+
     programs.thunderbird = {
       enable = true;
       profiles.default = {
