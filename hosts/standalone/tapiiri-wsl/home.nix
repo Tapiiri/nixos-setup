ffa1fa8d67d049db9635d23a6763c605fc65a0f2
Tapiiri <ilmari@tarpila.fi>
2026-04-02 17:28:16 +0300

    feat: add bash configuration to load session variables in every shell

--- hosts/standalone/tapiiri-wsl/home.nix
+++ hosts/standalone/tapiiri-wsl/home.nix
@@ -18,4 +18,19 @@
   home.sessionVariables = {
     NIXOS_SETUP_HM_PROFILE = "tapiiri-wsl";
   };
+
+  # Ensure home.sessionVariables are loaded in every shell.
+  programs.bash = {
+    enable = true;
+    profileExtra = ''
+      if [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      fi
+    '';
+    bashrcExtra = ''
+      if [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      fi
+    '';
+  };
 }
