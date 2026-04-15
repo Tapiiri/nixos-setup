3f0dd3cd75bedd66714ef7a189d4907e137ea200
Tapiiri <ilmari@tarpila.fi>
2026-04-15 11:32:28 +0300

    feat: clipboard support to zellij

--- home/modules/zellij.nix
+++ home/modules/zellij.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
@@ -8,6 +9,9 @@ in {
   options.my.zellij.enable = mkEnableOption "Zellij terminal multiplexer";
 
   config = mkIf config.my.zellij.enable {
+    # wl-clipboard provides wl-copy, which Zellij uses as copy_command on Wayland.
+    home.packages = [pkgs.wl-clipboard];
+
     programs.zellij = {
       enable = true;
       enableBashIntegration = config.my.shell.bash.enable;
